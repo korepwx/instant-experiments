0833514d8dd9453fef1bc0c248321cc6ecf4e610
Haowen Xu <public@korepwx.com>
2016-05-31 00:16:50 +0800

    change Softmax in tensorflow to sigmoid if num_units == 2

--- ipwxlearn/glue/tensorflow/layers/softmax.py
+++ ipwxlearn/glue/tensorflow/layers/softmax.py
@@ -23,14 +23,27 @@ class SoftmaxLayer(DenseLayer):
     """
 
     def __init__(self, name, incoming, num_units, W=init.XavierNormal(), b=init.Constant(0.)):
+        # if the target num is only 2, we use sigmoid instead of softmax.
+        true_num_units = num_units if num_units > 2 else 1
         super(SoftmaxLayer, self).__init__(
-            name=name, incoming=incoming, num_units=num_units, W=W, b=b, nonlinearity=None)
+            name=name, incoming=incoming, num_units=true_num_units, W=W, b=b, nonlinearity=None)
+
+        # although we might actually use sigmoid with 1 output, we still want the output shape to be 2.
+        self.num_units = num_units
+
+    def get_output_from_logits(self, logits):
+        if self.num_units == 2:
+            output = tf.nn.sigmoid(logits)
+            output = tf.concat(1, [output, 1.0 - output])
+        else:
+            output = tf.nn.softmax(logits)
+        return output
 
     def get_output_for(self, input, **kwargs):
         logits = kwargs.get('logits', False)
         output = super(SoftmaxLayer, self).get_output_for(input, **kwargs)
         if not logits:
-            output = tf.nn.softmax(output)
+            output = self.get_output_from_logits(output)
         return output
 
 
@@ -53,8 +66,11 @@ def get_output_with_sparse_softmax_crossentropy(layer, labels, inputs=None, **kw
     kwargs_logits = kwargs.copy()
     kwargs_logits['logits'] = True
     logits = get_output(layer, inputs=inputs, **kwargs_logits)
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels)
 
-    # compose the real output by applying softmax to the logits.
-    output = tf.nn.softmax(logits)
-    return output, loss
+    # compute loss, and compose the real output by applying softmax to the logits.
+    if layer.num_units == 2:
+        loss = tf.nn.sigmoid_cross_entropy_with_logits(logits, labels)
+    else:
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels)
+
+    return layer.get_output_from_logits(logits), loss
--- ipwxlearn/tests/glue/layers/test_softmax.py
+++ ipwxlearn/tests/glue/layers/test_softmax.py
@@ -2,6 +2,7 @@
 import unittest
 
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 from ipwxlearn import glue
 from ipwxlearn.glue import G
@@ -11,21 +12,38 @@ class SoftmaxUnitTest(unittest.TestCase):
 
     @staticmethod
     def make_softmax_data(n=10000, dim=10, target_num=2, dtype=np.float64):
-        W = (np.random.random([dim, target_num]) - 0.5).astype(dtype)
-        b = (np.random.random([target_num]) - 0.5).astype(dtype)
-        X = ((np.random.random([n, dim]) - 0.5) * 10.0).astype(dtype)
-        y = np.argmax(np.dot(X, W) + b, axis=1)
-        return (W, b), (X, y)
+        if target_num > 2 or glue.config.backend != 'tensorflow':
+            W = (np.random.random([dim, target_num]) - 0.5).astype(dtype)
+            b = (np.random.random([target_num]) - 0.5).astype(dtype)
+            X = ((np.random.random([n, dim]) - 0.5) * 10.0).astype(dtype)
+            y = np.argmax(np.dot(X, W) + b, axis=1)
+            return (W, b), (X, y)
+        else:
+            W = (np.random.random([dim, 1]) - 0.5).astype(dtype)
+            b = (np.random.random([1]) - 0.5).astype(dtype)
+            X = ((np.random.random([n, dim]) - 0.5) * 10.0).astype(dtype)
+            logits = (np.dot(X, W) + b).reshape([X.shape[0]])
+            y = (1.0 / (1 + np.exp(-logits)) <= 0.5).astype(np.int32)
+            return (W, b), (X, y)
 
     def test_binary_predicting(self):
         """Test binary softmax classifier."""
-        (W, b), (X, y) = self.make_softmax_data(target_num=2, dtype=glue.config.floatX)
+        target_num = 2
+        (W, b), (X, y) = self.make_softmax_data(target_num=target_num, dtype=glue.config.floatX)
+
+        # When target_num == 2, LogisticRegression from scikit-learn uses sigmoid, but lasagne doesn't.
+        # So we just test the consistency between scikit-learn LR with tensorflow backend.
+        if glue.config.backend == 'tensorflow':
+            lr = LogisticRegression().fit(X, y)
+            lr.coef_ = -W.T
+            lr.intercept_ = -b
+            self.assertTrue(np.alltrue(lr.predict(X) == y))
 
         graph = G.Graph()
         with graph.as_default():
             input_var = G.make_placeholder('inputs', shape=(None, W.shape[0]), dtype=glue.config.floatX)
             input_layer = G.layers.InputLayer(input_var, shape=(None, W.shape[0]))
-            softmax_layer = G.layers.SoftmaxLayer('softmax', input_layer, num_units=b.shape[0], W=W, b=b)
+            softmax_layer = G.layers.SoftmaxLayer('softmax', input_layer, num_units=target_num, W=W, b=b)
             predict_prob = G.layers.get_output(softmax_layer)
             predict_label = G.op.argmax(predict_prob, axis=1)
             predict_fn = G.make_function(inputs=[input_var], outputs=[predict_prob, predict_label])
@@ -33,16 +51,24 @@ class SoftmaxUnitTest(unittest.TestCase):
         with G.Session(graph):
             prob, predict = predict_fn(X)
             self.assertTrue(np.alltrue(predict == y))
+            if glue.config.backend == 'tensorflow':
+                self.assertTrue(np.allclose(lr.predict_proba(X), prob))
 
     def test_categorical_predicting(self):
         """Test categorical softmax classifier."""
-        (W, b), (X, y) = self.make_softmax_data(target_num=5, dtype=glue.config.floatX)
+        target_num = 5
+        (W, b), (X, y) = self.make_softmax_data(target_num=target_num, dtype=glue.config.floatX)
+
+        lr = LogisticRegression(multi_class='multinomial', solver='lbfgs').fit(X, y)
+        lr.coef_ = W.T
+        lr.intercept_ = b
+        self.assertTrue(np.alltrue(lr.predict(X) == y))
 
         graph = G.Graph()
         with graph.as_default():
             input_var = G.make_placeholder('inputs', shape=(None, W.shape[0]), dtype=glue.config.floatX)
             input_layer = G.layers.InputLayer(input_var, shape=(None, W.shape[0]))
-            softmax_layer = G.layers.SoftmaxLayer('softmax', input_layer, num_units=b.shape[0], W=W, b=b)
+            softmax_layer = G.layers.SoftmaxLayer('softmax', input_layer, num_units=target_num, W=W, b=b)
             predict_prob = G.layers.get_output(softmax_layer)
             predict_label = G.op.argmax(predict_prob, axis=1)
             predict_fn = G.make_function(inputs=[input_var], outputs=[predict_prob, predict_label])
@@ -50,3 +76,4 @@ class SoftmaxUnitTest(unittest.TestCase):
         with G.Session(graph):
             prob, predict = predict_fn(X)
             self.assertTrue(np.alltrue(predict == y))
+            self.assertTrue(np.allclose(lr.predict_proba(X), prob))
