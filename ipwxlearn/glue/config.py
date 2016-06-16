796b0aa0f1c62d38dcf3923701880fa64cc004ec
Haowen Xu <public@korepwx.com>
2016-06-17 01:07:39 +0800

    minor update

--- ipwxlearn/glue/config.py
+++ ipwxlearn/glue/config.py
@@ -2,12 +2,22 @@
 from __future__ import absolute_import
 
 import os
+import re
 import warnings
 
 
 # Read the config of default backend type.
 def _read_backend_type():
-    return os.environ.get('TENSOR_BACKEND', 'theano').lower()
+    backend = os.environ.get('TENSOR_BACKEND', 'theano').lower()
+    if backend == 'tensorflow':
+        # TensorFlow would not perform correctly if GPU is selected by Theano.
+        # So we unset the THEANO_FLAGS if TensorFlow is going to be used.
+        theano_flags = ''.join([
+            kv for kv in os.environ.get('THEANO_FLAGS', '').split(',')
+            if not re.match(r'^device=gpu.*$', re.I)
+        ])
+        os.environ['THEANO_FLAGS'] = theano_flags
+    return backend
 
 backend = _read_backend_type()
 
