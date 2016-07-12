5d7841aef636ab034988d56d8452c0cd3a5fb19f
Haowen Xu <public@korepwx.com>
2016-07-12 19:49:09 +0800

    minor update

--- ipwxlearn/models/noise/__init__.py
+++ ipwxlearn/models/noise/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from ipwxlearn.glue import G
 
+__all__ = [
+    'NoiseGenerator',
+    'DropoutNoise',
+    'GaussianNoise',
+]
+
 
 class NoiseGenerator(object):
     """
@@ -28,3 +34,23 @@ class DropoutNoise(NoiseGenerator):
 
     def add_noise(self, x):
         return G.random.binomial(shape=G.op.shape(x), p=1. - self.p, n=1, dtype=x.dtype) * x
+
+
+class GaussianNoise(NoiseGenerator):
+    """
+    Noise generator that randomly add gaussian noise with determined mean and variance to the input tensor.
+
+    Since this noise generator will add i.i.d. gaussian random noise to every dimension of the input tensor,
+    you might need to normalize the input dimensions first.
+
+    :param mean: Mean of the gaussian noise. (Default 0.0)
+    :param stddev: Standard derivation of the gaussian noise. (Default 1.0)
+    """
+
+    def __init__(self, mean=0.0, stddev=1.0):
+        self.mean = mean
+        self.stddev = stddev
+
+    def add_noise(self, x):
+        return G.random.normal(shape=G.op.shape(x), mean=self.mean, stddev=self.stddev, dtyhpe=x.dtype) + x
+
